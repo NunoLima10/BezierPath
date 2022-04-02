330f05c7e18fb96a5d93861e83ef549fbf2fb38a
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-04-02 20:45:33 -0100

    adicionando quadratic bezier

--- dots_suporte.py
+++ dots_suporte.py
@@ -1,7 +1,19 @@
+from tkinter import X
 import pygame
 from pygame.constants import MOUSEBUTTONDOWN,MOUSEBUTTONUP
 
-
+def mouse_clik(event ,dots, mouse_position) -> None:
+    if event.type==MOUSEBUTTONDOWN:
+        if event.button==1:
+            for dot in dots.values():
+                if dot.position_is_in(mouse_position):
+                    dot.set_over(True)
+        
+    if event.type==MOUSEBUTTONUP:
+        if event.button==1:
+            for dot in dots.values():
+                if dot.position_is_in(mouse_position):
+                    dot.set_over(False)
 
 def dots_update(dots, surface, mouse_position) -> dict:
      
@@ -19,21 +31,48 @@ def dots_update(dots, surface, mouse_position) -> dict:
 
 def draw_dots_lines(dots, surface) -> None:
     pygame.draw.line(surface,(0,255,0),dots["P0"].position,dots["P1"].position)
-    pygame.draw.line(surface,(0,255,0),dots["P2"].position,dots["P1"].position) 
+    pygame.draw.line(surface,(0,255,0),dots["P2"].position,dots["P1"].position)
+
+    t =0
+    t_next=0.01
+    while(1 > t_next):
+        point1 = quadratic_bezier(dots,t) 
+        point2 = quadratic_bezier(dots,t_next)
+
+        pygame.draw.line(surface,(255,255,255),point1,point2)
+
+        t+=0.01
+        t_next+=0.01
 
 
-def mouse_clik(event,dots,mouse_position) -> None:
-    if event.type==MOUSEBUTTONDOWN:
-        if event.button==1:
-            for dot in dots.values():
-                if dot.position_is_in(mouse_position):
-                    dot.set_over(True)
-        
-    if event.type==MOUSEBUTTONUP:
-        if event.button==1:
-            for dot in dots.values():
-                if dot.over:
-                    dot.set_over(False)
+
+
+
+def quadratic_bezier(dots,t):
+    pos_P0 = dots["P0"].position
+    pos_P1 = dots["P1"].position
+    pos_P2 = dots["P2"].position
+    
+    x_final = (1 -t) ** 2 * pos_P0[0] + (1 -t) * 2 * t * pos_P1[0] + t * t * pos_P2[0]
+    y_final = (1 -t) ** 2 * pos_P0[1] + (1 -t) * 2 * t * pos_P1[1] + t * t * pos_P2[1]
+
+    return [x_final,y_final]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             
 
 
